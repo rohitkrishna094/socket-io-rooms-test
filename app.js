01c9b34b5205a0b7936b76c58c40379508f2979f
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-02-12 21:59:45 -0500

    Added npm start script to package.json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
 
-server.listen(8080);
+server.listen(8080, () => console.log('connected on 8080'));
 
 // routing
 app.get('/', function (req, res) {
